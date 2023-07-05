806e156e9309c116ef0064c5130bdfd85a92c7ba
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-05 13:15:01 -0300

    feat: added Recommended Trip and TripItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Image from 'next/image';
 import TripSearch from './components/TripSearch';
 import QuickSearch from './components/QuickSearch';
+import RecommendedTrips from './components/RecommendedTrips';
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   )
 }
