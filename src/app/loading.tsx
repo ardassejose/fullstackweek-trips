fda964fb0bb5e7cf068d507157f8b6c571be9421
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-05 13:55:36 -0300

    chore: loading screen updated for simples text

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>loading...</div>
+    <div className='container mx-auto flex justify-center items-center text-center'>Loading...</div>
   )
 }
