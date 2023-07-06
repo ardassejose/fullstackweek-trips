33ab9f1cb476a5d22b91aeee1d7e4bf0f8cb2ea2
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-06 11:54:17 -0300

    style: add border to trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -10,21 +10,21 @@ interface TripReservationProps {
 
 export default function TripReservation({ trip }: TripReservationProps) {
   return (
-    <div>
-      <div className="flex flex-col px-5">
-        <div className="flex gap-4">
-          <DatePicker placeholderText="Data de Início" onChange={() => { }} className="w-full" />
-          <DatePicker placeholderText="Data final" onChange={() => { }} className="w-full" />
-        </div>
+    <div className="flex flex-col px-5">
+      <div className="flex gap-4">
+        <DatePicker placeholderText="Data de Início" onChange={() => { }} className="w-full" />
+        <DatePicker placeholderText="Data final" onChange={() => { }} className="w-full" />
+      </div>
 
-        <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
+      <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
 
-        <div className="flex justify-between mt-2">
-          <p className="font-medium text-sm text-primaryDark">Total:</p>
-          <p className="font-medium text-sm text-primaryDark">R$2500</p>
-        </div>
+      <div className="flex justify-between mt-2">
+        <p className="font-medium text-sm text-primaryDark">Total:</p>
+        <p className="font-medium text-sm text-primaryDark">R$2500</p>
+      </div>
 
-        <Button className="mt-3">Reservar agrora</Button>
+      <div className="pb-10 w-full border-b-graySecondary">
+        <Button className="mt-3 w-full">Reservar agrora</Button>
       </div>
     </div>
   )
