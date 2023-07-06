9e13dd1a71b0a51ea96e45cd89971211bebfd571
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-06 15:25:34 -0300

    feat: add date validation to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -1,72 +1,117 @@
 'use client'
-import Button from "@/components/Button";
-import DatePicker from "@/components/DatePicker";
-import Input from "@/components/Input";
-import { Trip } from "@prisma/client";
-import { Controller, useForm } from "react-hook-form";
+import Button from '@/components/Button'
+import DatePicker from '@/components/DatePicker'
+import Input from '@/components/Input'
+import { Trip } from '@prisma/client'
+import { Controller, useForm } from 'react-hook-form'
 
 interface TripReservationProps {
-  trip: Trip;
+  tripStartDate: Date
+  tripEndDate: Date
+  maxGuests: number
 }
 
 interface TripReservationForm {
-  guests: number;
+  guests: number
   startDate: Date | null
   endDate: Date | null
 }
 
-export default function TripReservation({ trip }: TripReservationProps) {
-  const { register, handleSubmit, formState: { errors }, control } = useForm<TripReservationForm>()
+export default function TripReservation({
+  tripStartDate,
+  tripEndDate,
+  maxGuests
+}: TripReservationProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    watch
+  } = useForm<TripReservationForm>()
 
-  const onSubmit = (data: any) => { console.log({ data }) }
+  const onSubmit = (data: any) => {
+    console.log({ data })
+  }
+
+  const startDate = watch('startDate')
 
   return (
-    <div className="flex flex-col px-5">
-      <div className="flex gap-4">
+    <div className='flex flex-col px-5'>
+      <div className='flex gap-4'>
+        {/* Data de ida */}
         <Controller
-          name="startDate"
+          name='startDate'
           rules={{
             required: {
               value: true,
-              message: "Informe a data de ida."
+              message: 'Informe a data de ida.',
             },
           }}
           control={control}
-          render={({ field }) => <DatePicker error={!!errors?.startDate} errorMessage={errors?.startDate?.message} onChange={field.onChange} selected={field.value} placeholderText="Data de Início" className="w-full" />}
+          render={({ field }) => (
+            <DatePicker
+              error={!!errors?.startDate}
+              errorMessage={errors?.startDate?.message}
+              onChange={field.onChange}
+              selected={field.value}
+              placeholderText='Data de Início'
+              className='w-full'
+              minDate={tripStartDate}
+            />
+          )}
         />
 
+        {/* Data de volta */}
         <Controller
-          name="endDate"
+          name='endDate'
           rules={{
             required: {
               value: true,
-              message: "Informe a data de volta."
+              message: 'Informe a data de volta.',
             },
           }}
           control={control}
-          render={({ field }) => <DatePicker error={!!errors?.endDate} errorMessage={errors?.endDate?.message} onChange={field.onChange} selected={field.value} placeholderText="Data final" className="w-full" />}
+          render={({ field }) => (
+            <DatePicker
+              error={!!errors?.endDate}
+              errorMessage={errors?.endDate?.message}
+              onChange={field.onChange}
+              selected={field.value}
+              placeholderText='Data final'
+              className='w-full'
+              maxDate={tripEndDate}
+              minDate={startDate ?? tripStartDate}
+            />
+          )}
         />
-
       </div>
 
-      <Input {...register("guests", {
-        required: {
-          value: true,
-          message: "Informe o número de hóspedes."
-        }
-      })} placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
-        className="mt-4"
+      <Input
+        {...register('guests', {
+          required: {
+            value: true,
+            message: 'Informe o número de hóspedes.',
+          },
+        })}
+        placeholder={`Número de hóspedes (max: ${maxGuests})`}
+        className='mt-4'
         error={!!errors?.guests}
         errorMessage={errors?.guests?.message}
       />
 
-      <div className="flex justify-between mt-2">
-        <p className="font-medium text-sm text-primaryDark">Total:</p>
-        <p className="font-medium text-sm text-primaryDark">R$2500</p>
+      <div className='flex justify-between mt-2'>
+        <p className='font-medium text-sm text-primaryDark'>Total:</p>
+        <p className='font-medium text-sm text-primaryDark'>R$2500</p>
       </div>
 
-      <div className="pb-10 w-full border-b-graySecondary">
-        <Button onClick={() => handleSubmit(onSubmit)()} variant="primary" className="mt-3 w-full">Reservar agrora</Button>
+      <div className='pb-10 w-full border-b-graySecondary'>
+        <Button
+          onClick={() => handleSubmit(onSubmit)()}
+          variant='primary'
+          className='mt-3 w-full'>
+          Reservar agrora
+        </Button>
       </div>
     </div>
   )
