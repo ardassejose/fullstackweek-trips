e30551c69b952f29818797889ceac9dfac1b2884
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-06 12:27:18 -0300

    fix: prisma doesn't hot reload anymore

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = 
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"]
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
