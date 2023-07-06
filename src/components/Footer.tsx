d728076768b5ae185e4c4eecace9bba7f96f2dce
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-07-06 13:31:43 -0300

    style: add gray-100 bg color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <div className="bg-walterWhite p-5 justify-center flex flex-col items-center">
+    <div className="bg-gray-100 p-5 justify-center flex flex-col items-center">
       <Image src="/logo.png" width={133} height={23} alt="Full Stack Week" />
       <p className="text-sm font-medium mt-1 text-primaryDarker">Todos os direitos reservados.</p>
     </div>
